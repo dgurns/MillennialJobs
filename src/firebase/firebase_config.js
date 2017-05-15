b8c55940d550aecc9b3294b047b33598ed20450c
Dan Gurney <gurney.dan@gmail.com>
2017-05-15 16:26:33 -0400

    Add firebase config details

---
+++ src/firebase/firebase_config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDrSP8FQD_t0dhoJ3cDH9lcoLw49uW5hXo',
+  authDomain: 'millennialjobs-7853c.firebaseapp.com',
+  databaseURL: 'https://millennialjobs-7853c.firebaseio.com',
+  projectId: 'millennialjobs-7853c',
+  storageBucket: 'millennialjobs-7853c.appspot.com',
+  messagingSenderId: '912033390464'
+};
