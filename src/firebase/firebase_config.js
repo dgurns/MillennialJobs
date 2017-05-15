637a445da641e980a93768adea97be20807233b0
Dan Gurney <gurney.dan@gmail.com>
2017-05-15 16:31:28 -0400

    Trying gitignore again

--- src/firebase/firebase_config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyDrSP8FQD_t0dhoJ3cDH9lcoLw49uW5hXo',
-  authDomain: 'millennialjobs-7853c.firebaseapp.com',
-  databaseURL: 'https://millennialjobs-7853c.firebaseio.com',
-  projectId: 'millennialjobs-7853c',
-  storageBucket: 'millennialjobs-7853c.appspot.com',
-  messagingSenderId: '912033390464'
-};
