f13bcf6275351b9b5c82b2a41ab44fa3e2363e69
Dan Gurney <gurney.dan@gmail.com>
2017-05-24 12:13:00 -0400

    Rough in RadioButton component

---
+++ src/components/RadioButton.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { TouchableWithoutFeedback, Text, View, StyleSheet } from 'react-native';
+import * as constants from '../constants';
+
+class RadioButton extends Component {
+  static defaultProps = {
+    onPress: () => {},
+    labelText: '',
+    selected: false
+  }
+
+  render() {
+    const { radioButtonContainer, radioButton, labelText } = styles;
+
+    return (
+      <TouchableWithoutFeedback onPress={this.props.onPress}>
+        <View style={radioButtonContainer}>
+          <View
+            style={[radioButton, this.props.selected ? { backgroundColor: constants.GREEN_COLOR } : '']}
+          />
+          <Text
+            style={[labelText, this.props.selected ? { color: constants.GREEN_COLOR } : '']}
+          >
+            {this.props.labelText}
+          </Text>
+        </View>
+      </TouchableWithoutFeedback>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  radioButtonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    marginBottom: 20
+  },
+  radioButton: {
+    height: 35,
+    width: 35,
+    borderRadius: 18,
+    borderColor: constants.DARK_GRAY_COLOR,
+    borderWidth: 1,
+    marginRight: 10
+  },
+  labelText: {
+    fontSize: constants.BODY_FONT_SIZE,
+    color: constants.BLACK_COLOR
+  }
+});
+
+export default RadioButton;
--- src/screens/InterestsScreen.js
+++ src/screens/InterestsScreen.js
@@ -1,12 +1,64 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import * as constants from '../constants';
+import Button from '../components/Button';
+import RadioButton from '../components/RadioButton';
 
 class InterestsScreen extends Component {
   render() {
+    const { title, interestsListContainer, button } = styles;
+
     return (
-      <View />
+      <View style={{ flex: 1 }}>
+        <Text style={title}>
+          What are you interested in?
+        </Text>
+        <View style={interestsListContainer}>
+          <RadioButton
+            labelText='Development'
+            selected={false}
+          />
+          <RadioButton
+            labelText='Construction'
+            selected
+          />
+          <RadioButton
+            labelText='Photography'
+            selected={false}
+          />
+        </View>
+        <View style={button}>
+          <Button
+            buttonText='Yes'
+          />
+        </View>
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: constants.TITLE_FONT_SIZE,
+    color: constants.BLACK_COLOR,
+    textAlign: 'center',
+    paddingLeft: 20,
+    paddingRight: 20
+  },
+  interestsListContainer: {
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    padding: 50
+  },
+  button: {
+    zIndex: 50,
+    position: 'absolute',
+    flex: 1,
+    left: 0,
+    right: 0,
+    bottom: 170
+  }
+});
 
 export default InterestsScreen;
