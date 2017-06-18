46899a998ca10c1e3638b61cbb502b36f2a579bc
Dan Gurney <gurney.dan@gmail.com>
2017-06-18 18:47:11 -0400

    SplashScreen is working on Android now.

--- android/app/src/main/java/com/millennialjobs/MainActivity.java
+++ android/app/src/main/java/com/millennialjobs/MainActivity.java
@@ -1,6 +1,9 @@
 package com.millennialjobs;
 
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
+import com.cboy.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
@@ -8,8 +11,15 @@ public class MainActivity extends ReactActivity {
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
      */
+
     @Override
     protected String getMainComponentName() {
         return "MillennialJobs";
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+      SplashScreen.show(this);  // here
+      super.onCreate(savedInstanceState);
     }
 }
